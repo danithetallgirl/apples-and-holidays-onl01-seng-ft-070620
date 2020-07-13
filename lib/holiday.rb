2e520cbf59fac06613f14cc12fa1adbeba16de34
Dani Bassett <dani@danithetallgirl.com>
2020-07-13 03:24:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,10 +41,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_supplies)
-  winter.each do |key, supply|
-    supply
-end
-all_winter_holiday_supplies(holiday_supplies)
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
