29ddeacefbada61adba4ca1c5184db60de094a97
Dani Bassett <dani@danithetallgirl.com>
2020-07-13 03:23:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,9 +36,8 @@ holiday_supplies[:spring][:memorial_day] << "#{supply}"
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-#all_holidays_with_bbq(holiday_supplies)
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_supplies)
